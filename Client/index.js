d855eeab6de086f49c74e10ff3c60d46ff296d87
Dung-Vu1 <86118834+Dung-Vu@users.noreply.github.com>
2022-07-12 11:48:18 +0700

    update client

--- Client/index.js
+++ Client/index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Welcome from './screens/Welcome'
+import React from 'react';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => <Welcome/>);
---
+++ Client/screens/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { 
+    Text,
+    View,
+    Image,
+    ImageBackground,
+} from "react-native";
+import {icons,images,} from '../'
