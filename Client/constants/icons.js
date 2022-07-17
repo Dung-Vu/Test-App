b60be9d49b829733850970427690bb95f31db7c8
Dung-Vu1 <86118834+Dung-Vu@users.noreply.github.com>
2022-07-17 15:00:32 +0700

    complete picture one

--- Client/constants/icons.js
+++ Client/constants/icons.js
@@ -1,3 +1,12 @@
 export default {
-    
+    search: require('../assets/search.png'),
+    dentist : require('../assets/dentist.png'),
+    heart : require('../assets/heart.png'),
+    neurology : require('../assets/neurology.png'),
+    orthopedic : require('../assets/orthopedic.png'),
+    star : require('../assets/star.png'),
+    stethoscope : require('../assets/stethoscope.png'),
+    chat : require('../assets/chat.png'),
+    calendar : require('../assets/calendar.png'),
+    account : require('../assets/account.png'),
 }
--- Client/screens/Welcome.js
+++ Client/screens/Welcome.js
@@ -1,71 +1,214 @@
-import React from "react";
-import {
-    Text,
-    View,
-    Image,
-    ImageBackground,
-
-} from "react-native";
-import { Header } from "react-native/Libraries/NewAppScreen";
-import { icons, images } from '../constants'
+import React from 'react';
+import { Text, View, Image, ImageBackground, StyleSheet, } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import { icons, images } from '../constants';
 
 function Welcome(props) {
-    const TextHearder = "Hi,\nSaepul Nahwan"
-    return <View style={{
-        flex: 100,
-    }}>
-        <View style={{
-            flex: 50,
-        }}>
-            <View style={{
-                backgroundColor: '#3CBAF0',
-                borderBottomRightRadius: 60,
-            }}>
-                <View style={{
-                    flexDirection: 'row',
-                    // alignItems: 'center',
-                    // justifyContent: "flex-start",
-                    marginTop:17,
+    const styles = StyleSheet.create({
+        titleHeader: { fontSize: 32, fontWeight: 'bold', color: 'white' },
+        styleIcon: {
+            width: 84,
+            height: 84,
+            backgroundColor: '#3CBAF0',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            marginRight: 10,
+        },
+        sizeIcon: { width: 26, height: 30, },
+        styleIconBottom: {
+            width: 80,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight:30,
+        },
+        sizeIconBottom: { width: 28, height:28, },
+    });
+    return (
+        <View style={{ flex: 1 }}>
+            <View
+                style={{
+                    height: '50%',
+                    backgroundColor: '#3CBAF0',
+                    borderBottomRightRadius: 60,
                 }}>
-                    <Text style={{
-                        color: 'white',
-                        left: 27,
-                    }}>Hi,</Text>
-                    <Text style={{
-                        color: 'white',
-                        fontWeight:"bold",
-                        top:30,
-                    }}>Saepul Nahwan</Text>
-                    <Image source={images.people}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        // justifyContent: "flex-start",
+                        marginTop: 17,
+                    }}>
+                    <Text
                         style={{
-                            marginLeft:230,
+                            color: 'white',
+                            left: 27,
+                        }}>
+                        Hi,
+                    </Text>
+                    <Text
+                        style={{
+                            color: 'white',
+                            fontWeight: 'bold',
+                            top: 30,
+                        }}>
+                        Saepul Nahwan
+                    </Text>
+                    <Image
+                        source={images.people}
+                        style={{
+                            top: 15,
+                            marginLeft: 240,
                             width: 34.87,
                             height: 34.87,
+                        }}></Image>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{
+                        marginLeft: 60,
+                        top: 130,
+                    }}>
+                        <Text style={styles.titleHeader}>Find Your</Text>
+                        <Text style={styles.titleHeader}>Desired</Text>
+                        <Text style={styles.titleHeader}>Doctor</Text>
+                    </View>
+                    <Image
+                        source={images.doctor}
+                        style={{
+                            top: 56,
+                            marginLeft: -8,
+                            width: 243,
+                            height: 288,
+                            borderBottomRightRadius: 60,
+                        }}></Image>
+                </View>
+                <View style={{
+                    backgroundColor: 'white',
+                    width: 320,
+                    height: 50,
+                    // justifyContent: 'center',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    borderRadius: 34,
+                    left: 55,
+                    bottom: 18,
+                }}>
+                    <Image
+                        source={icons.search}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            left: 20,
                         }}
                     ></Image>
+                    <Text style={{ color: '#475464', left: 40, }}>Find a Doctor or Specialist ...</Text></View>
+            </View>
+            <View style={{ height: '40%', backgroundColor: '#FBFCFF', }}>
+                <View style={{
+                    flexDirection: 'row',
+                    marginTop: 24,
+                    marginLeft: 32,
+                }}>
+                    <View style={styles.styleIcon}>
+                        <Image
+                            source={icons.dentist}
+                            style={styles.sizeIcon}
+                        ></Image>
+                        <Text>Dennist</Text>
+                    </View>
+                    <View style={styles.styleIcon}>
+                        <Image
+                            source={icons.heart}
+                            style={styles.sizeIcon}
+                        ></Image>
+                        <Text>Heart</Text>
+                    </View>
+                    <View style={styles.styleIcon}>
+                        <Image
+                            source={icons.neurology}
+                            style={styles.sizeIcon}
+                        ></Image>
+                        <Text>Neurology</Text>
+                    </View>
+                    <View style={styles.styleIcon}>
+                        <Image
+                            source={icons.orthopedic}
+                            style={styles.sizeIcon}
+                        ></Image>
+                        <Text>Orthopedic</Text>
+                    </View>
                 </View>
-                <Image source={images.doctor}
-                    style={{
-                        marginLeft: 170,
-                        width: 243,
-                        height: 288,
-                        borderBottomRightRadius: 60,
-                    }}
-                ></Image>
+                <View style={{ flexDirection: 'row', marginTop: 30, marginLeft: 27, }}>
+                    <Text style={{ fontWeight: '700', }}>Top Rated Doctor</Text>
+                    <Text style={{ marginLeft: 170, color: '#FF9972' }}>View All</Text>
+                </View>
+                <View style={{ flexDirection: 'row', padding: 27 }}>
+                    <View style={{ flexDirection: 'row', }}>
+                        <ImageBackground style={{ backgroundColor: '#319CE3', borderRadius: 40, }}>
+                            <Image
+                                source={images.doctor}
+                                style={{
+                                    width: 76,
+                                    height: 70,
+                                    borderRadius: 40,
+                                }}
+                            ></Image>
+                        </ImageBackground>
+                        <View style={{ backgroundColor: '#0CDD6C', width: 12, height: 11, borderRadius: 30, marginTop: 59, marginLeft: -20, }}></View>
+                    </View>
+                    <View style={{ justifyContent: 'center', marginLeft: 30, }}>
+                        <Text style={{ fontWeight: '700' }}>dr. Kathryn Murphy</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                            <Image source={icons.dentist} style={{ width: 14, height: 14, }}></Image>
+                            <Text style={{ marginLeft: 6, fontSize: 12, fontWeight: '400', }}>Dentist - Stillwater Clinic</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                            <Image source={icons.star} style={{ height: 10, width: 10, marginLeft: 2, }}></Image>
+                            <Image source={icons.star} style={{ height: 10, width: 10, marginLeft: 2, }}></Image>
+                            <Image source={icons.star} style={{ height: 10, width: 10, marginLeft: 2, }}></Image>
+                            <Image source={icons.star} style={{ height: 10, width: 10, marginLeft: 2, }}></Image>
+                            <Image source={icons.star} style={{ height: 10, width: 10, marginLeft: 2, }}></Image>
+                            <Text style={{ marginLeft: 8, fontSize: 12, fontWeight: '400', }}>(1541 Ratings)</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={{
+                height: '10%',
+                flexDirection: 'row',
+            }}>
+                <View style={styles.styleIconBottom}>
+                    <Image
+                        source={icons.stethoscope}
+                        style={styles.sizeIconBottom}
+                    ></Image>
+                    <Text style={{ fontSize: 10, fontWeight: '400', }}>Appointment</Text>
+                </View>
+                <View style={styles.styleIconBottom}>
+                    <Image
+                        source={icons.chat}
+                        style={styles.sizeIconBottom}
+                    ></Image>
+                    <Text style={{ fontSize: 10, fontWeight: '400', }}>Chat</Text>
+                </View>
+                <View style={styles.styleIconBottom}>
+                    <Image
+                        source={icons.calendar}
+                        style={styles.sizeIconBottom}
+                    ></Image>
+                    <Text style={{ fontSize: 10, fontWeight: '400', }}>History</Text>
+                </View>
+                <View style={styles.styleIconBottom}>
+                    <Image
+                        source={icons.account}
+                        style={styles.sizeIconBottom}
+                    ></Image>
+                    <Text style={{ fontSize: 10, fontWeight: '400', }}>Profile</Text>
+                </View>
             </View>
         </View>
-        <View style={{
-            flex: 40,
-            backgroundColor: 'yellow'
-        }}>
 
-        </View>
-        <View style={{
-            flex: 10,
-            backgroundColor: 'red'
-        }}>
 
-        </View>
-    </View>
+    );
 }
-export default Welcome
+export default Welcome;
